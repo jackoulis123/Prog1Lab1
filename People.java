fbabdd9a10d7a697ce6ff256b9a24f4c04908776
Aristotelis <aris2013@outlook.com.gr>
2020-01-30 14:03:35 -0500

    initial commit

---
+++ People.java
@@ -0,0 +1,27 @@
+public class People{
+
+	public static void main(String[] args){
+
+System.out.println("\nHello,World,People!!!");
+
+	int year;
+	double distance;
+	char letter;
+	boolean state;
+
+	year = 2020
+	distance = 2.3
+	letter = 'a';
+	state = true;
+
+
+
+
+
+
+
+
+
+
+	}
+}
