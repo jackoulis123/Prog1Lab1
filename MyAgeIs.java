f719baaedc6115031dc3a1a8f7ab3506523abb58
Aristotelis Kondylis <aris2013@outlook.com.gr>
2020-02-04 09:23:25 -0500

    Lab1 Solution

---
+++ MyAgeIs.java
@@ -0,0 +1,15 @@
+public class MyAgeIs {
+
+    public static void main(String[] args) {
+
+    int currentYear, yearOfBirth, age;
+
+    currentYear = 2020;
+    yearOfBirth = 2000;
+    age = currentYear - yearOfBirth;
+
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("\nI will be " + age + " years old in a couple of days");
+    }
+
+}
