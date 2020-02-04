e0758bb0e6551d220b777d3281e96b0e143d5ab2
Aristotelis Kondylis <aris2013@outlook.com.gr>
2020-02-04 09:42:48 -0500

    Lab1 Solution2

---
+++ Distance.java
@@ -0,0 +1,23 @@
+public class Distance {
+
+    public static void main(String[] args) {
+    	double meters, feet, convert;
+    	meters = 1;
+    	feet = 3.28084;
+    	convert = meters * feet;
+
+
+
+
+        System.out.println("\n1 meter is equal to " + convert + " feet");
+        
+        
+
+
+
+
+
+        
+    }
+
+}
