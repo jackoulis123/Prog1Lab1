6d1abb6649778d6e0ec3569ec0c988177d4c9283
Aristotelis Kondylis <aris2013@outlook.com.gr>
2020-02-04 10:51:32 -0500

    Lab1 Solution3

---
+++ Rectangle.java
@@ -0,0 +1,15 @@
+public class Rectangle {
+
+    public static void main(String[] args) {
+    	double length, height, area, perimeter;
+    	length = 2;
+    	height = 5;
+    	perimeter = 2 * (length * height);
+    	area = length * height;
+
+
+System.out.println("\nIf the length is equal to " + length + " and the height is equal to " + height + " the perimeter is equal to " + perimeter);
+System.out.println("\nIf the length is equal to " + length + " and the height is equal to " + height + " the area is equal to " + area);
+    }
+
+}
